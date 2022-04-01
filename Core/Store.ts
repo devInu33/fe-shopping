ef64f4b5a7bf7c51be34e611e61aee8b05ce6edb
inust33 <inust33@gmail.com>
2022-04-01 14:32:43 +0900

    EDIT: store 초기화 수정

--- Core/Store.ts
+++ Core/Store.ts
@@ -1,72 +1,74 @@
-import View from "./View";
-import { stateObj } from "../util";
+import View from "./View.js";
+import {stateObj} from "../util.js";
 
 export class Store extends Map<string | symbol, Set<View>> {
-  static #state: stateObj;
-  state: stateObj;
-  static #storageKey: string = Symbol().toString();
-  static get storageKey(): string {
-    return this.#storageKey;
-  }
-
-  constructor(state = {}) {
-    super();
-    Store.#state = this.observe(state);
-    this.state = new Proxy(state, {
-      get: (target, name) => Store.#state[name],
-    });
-  }
-
-  addView(view: View) {
-    Object.entries(view.initState()).forEach(([key, value]) => {
-      Store.#state[key] = value;
-      this.subscribe(key, view);
-    });
-  }
-
-  subscribe(key: string | symbol, view: View) {
-    super.has(key)
-      ? // @ts-ignore
-        super.get(key).add(view)
-      : super.set(key, new Set<View>().add(view));
-  }
-
-  unsubscribe(key: string | symbol, view: View) {
-    if (!super.has(key)) return;
-    // @ts-ignore
-    super.get(key).delete(view);
-  }
-
-  observe(state: stateObj) {
-    // const isProxy = Symbol("isProxy");
-    // if (name === isProxy) return true;
-    // if (!prop.isProxy && typeof prop == "object")
-    //   return new Proxy(prop, handler);
-    const handler: ProxyHandler<any> = {
-      get: (target, name, receiver) => {
-        const prop = Reflect.get(target, name, receiver);
-        if (typeof prop === "undefined") return;
-        return prop;
-      },
-      set: (target, name, value) => {
-        if (target[name] == value) return true;
-        Reflect.set(target, name, value);
-        if (super.has(name)) {
-          // @ts-ignore
-          super.get(name).forEach((view) => {
-            view.render();
-          });
-        }
-        return true;
-      },
-    };
-    return new Proxy(state, handler);
-  }
-
-  setState(newState: stateObj) {
-    for (const [key, value] of Object.entries(newState)) {
-      if (!(key in Store.#state)) continue;
-      Store.#state[key] = value;
+    static #state: Record<string | symbol, any>;
+    static state: Record<string | symbol, any>;
+    private static _storageKey: string = Symbol().toString();
+    static get storageKey(): string {
+        return this._storageKey;
     }
-  }
+
+    constructor(state = {}) {
+        super();
+        if (!Store.#state) {
+            Store.#state = this.observe(state);
+            Store.state = new Proxy(state, {
+                get: (target, name) => Store.#state[name],
+            });
+        }
+    }
+
+    addView(view: View) {
+        Object.entries(view.initState()).forEach(([key, value]) => {
+            Store.#state[key] = value;
+            this.subscribe(key, view);
+        });
+    }
+
+    subscribe(key: string | symbol, view: View) {
+        super.has(key)
+            ? // @ts-ignore
+            super.get(key).add(view)
+            : super.set(key, new Set<View>().add(view));
+    }
+
+    unsubscribe(key: string | symbol, view: View) {
+        if (!super.has(key)) return;
+        // @ts-ignore
+        super.get(key).delete(view);
+    }
+
+    observe(state: stateObj) {
+        // const isProxy = Symbol("isProxy");
+        // if (name === isProxy) return true;
+        // if (!prop.isProxy && typeof prop == "object")
+        //   return new Proxy(prop, handler);
+        const handler: ProxyHandler<any> = {
+            get: (target, name, receiver) => {
+                const prop = Reflect.get(target, name, receiver);
+                if (typeof prop === "undefined") return;
+                return prop;
+            },
+            set: (target, name, value) => {
+                if (target[name] == value) return true;
+                Reflect.set(target, name, value);
+                if (super.has(name)) {
+                    // @ts-ignore
+                    super.get(name).forEach((view) => {
+                        view.render();
+                    });
+                }
+                return true;
+            },
+        };
+        return new Proxy(state, handler);
+    }
+
+    setState(newState: Record<string | symbol, any>) {
+        for (const [key, value] of Object.entries(newState)) {
+            if (!(key in Store.state)) continue;
+            Store.#state[key] = value;
+        }
+    }
 }
--- components/Megadrop.ts
+++ components/Megadrop.ts
@@ -1,82 +1,92 @@
 import View from "../Core/View.js";
-import { sources } from "../util.js";
+import {sources, stateObj} from "../util.js";
+
+type Layer = Record<string, Record<string, string[]> | string[]>;
 
 export class Megadrop extends View {
-  initState() {
-    return {
-      layers: sources["smartlayer"],
-      layerSelected: false,
-      firstIdx: -1,
-      secondIdx: -1,
-    };
-  }
+    initState() {
+        return {
+            layers: sources["smartlayer"],
+            layerSelected: false,
+            firstIdx: -1,
+            secondIdx: -1,
+        };
+    }
 
-  //3단 객체
-  //
-  template() {
-    const { layers, layerSelected, firstIdx, secondIdx } = this.state;
-    return `
+    //3단 객체
+    //
+    template() {
+        const {layers, layerSelected, firstIdx, secondIdx} = this.state;
+        return `
         <h3></h3>
         <ul class="shopping-layer" style=${
-          layerSelected ? "display:block" : "display:none"
+            layerSelected ? "display:block" : "display:none"
         }>
-        ${Object.entries(layers)
-          .map(
-            ([cat, sub], idx1) =>
-              `<li  data-idx=${idx1} ><a class="layer first">${cat}</a><div class="depth second" style=${
-                idx1 === firstIdx ? "display:block" : "display:none"
-              }><div><ul>${Object.entries(sub)
-                .map(
-                  ([cat, sub], idx2) =>
-                    `<li  data-idx=${idx2}><a class="layer second">${cat}</a><div class="depth third" style=${
-                      idx2 === secondIdx ? "display:block" : "display:none"
-                    }><ul>${sub
-                      .map((cat) => `<li class="layer third"><a>${cat}</a>`)
-                      .join("")}</ul></div></li>`
-                )
-                .join("")}</ul></div></div></li>`
-          )
-          .join("")}
+        ${Object.entries(layers as Layer)
+            .map(
+                ([cat, sub], idx1) =>
+                    `<li  data-idx=${idx1} ><a class="layer first">${cat}</a><div class="depth second" style=${
+                        idx1 === firstIdx ? "display:block" : "display:none"
+                    }><div><ul>${Object.entries(sub)
+                        .map(
+                            ([cat, sub], idx2) =>
+                                `<li  data-idx=${idx2}><a class="layer second">${cat}</a><div class="depth third" style=${
+                                    idx2 === secondIdx ? "display:block" : "display:none"
+                                }><ul>${sub
+                                    .map(
+                                        (cat: string) => `<li class="layer third"><a>${cat}</a>`
+                                    )
+                                    .join("")}</ul></div></li>`
+                        )
+                        .join("")}</ul></div></div></li>`
+            )
+            .join("")}
       </ul>
       `;
-  }
+    }
 
-  setEvent() {
-    this.addEvent(
-      "mouseenter",
-      ".layer.first",
-      ({ target }) => {
-        const idx = parseInt(target.closest("li[data-idx]").dataset.idx);
-        this.throttle(() => this.setState({ firstIdx: idx }), 100);
-      },
-      true
-    );
-    this.addEvent(
-      "mouseenter",
-      ".layer.second",
-      ({ target }) => {
-        const idx = parseInt(target.closest("li[data-idx]").dataset.idx);
-        this.throttle(() => this.setState({ secondIdx: idx }), 100);
-      },
-      true
-    );
-    this.addEvent(
-      "mouseleave",
-      ".layer.first",
-      ({ relatedTarget }) => {
-        if (relatedTarget.closest(".layer.first")) return false;
-        this.setState({ firstIdx: -1 });
-      },
-      true
-    );
-    this.addEvent(
-      "mouseleave",
-      ".layer.second",
-      ({ relatedTarget }) => {
-        if (relatedTarget.closest(".layer.second")) return false;
-        this.setState({ secondIdx: -1 });
-      },
-      true
-    );
-  }
+    setEvent() {
+        this.addEvent(
+            "mouseenter",
+            ".layer.first",
+            ({target}) => {
+                const idx = Number(
+                    ((target as HTMLElement).closest("li[data-idx]") as HTMLElement)
+                        ?.dataset.idx
+                );
+                this.throttle(() => this.setState({firstIdx: idx}), 100);
+            },
+            true
+        );
+        this.addEvent(
+            "mouseenter",
+            ".layer.second",
+            ({target}) => {
+                const idx = Number(
+                    ((target as HTMLElement).closest("li[data-idx]") as HTMLElement)
+                        ?.dataset.idx
+                );
+                this.throttle(() => this.setState({secondIdx: idx}), 100);
+            },
+            true
+        );
+        this.addEvent(
+            "mouseleave",
+            ".layer.first",
+            ({relatedTarget}) => {
+                if ((<HTMLElement>relatedTarget).closest(".layer.first")) return false;
+                this.setState({firstIdx: -1});
+            },
+            true
+        );
+        this.addEvent(
+            "mouseleave",
+            ".layer.second",
+            ({relatedTarget}) => {
+                if ((<HTMLElement>relatedTarget).closest(".layer.second")) return false;
+                this.setState({secondIdx: -1});
+            },
+            true
+        );
+    }
 }
